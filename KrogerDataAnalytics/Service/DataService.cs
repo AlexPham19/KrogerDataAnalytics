9e9c14ece93be2749f754e62f8f252578f4bcc4a
Quân Phạm (Alex Pham) <phamqh@mail.uc.edu>
2026-04-24 00:48:06 -0400

    search and filter is done

--- KrogerDataAnalytics/Service/DataService.cs
+++ KrogerDataAnalytics/Service/DataService.cs
@@ -12,6 +12,38 @@ namespace KrogerDataAnalytics.Service
             _dbFactory = dbFactory;
         }
 
+        public async Task<List<JoinedTransaction>> GetDataPullAsync()
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            return await (from t in context.Transactions
+                          join h in context.Households on t.Hshd_num equals h.Hshd_num
+                          join p in context.Products on t.Product_num equals p.Product_num
+                          orderby t.Hshd_num, t.Basket_num, t.Date, t.Product_num, p.Department, p.Commodity
+                          select new JoinedTransaction
+                          {
+                              Hshd_num = t.Hshd_num,
+                              Basket_num = t.Basket_num,
+                              Date = t.Date ?? DateTime.MinValue,
+                              Product_num = t.Product_num,
+                              Spend = t.Spend ?? 0,
+                              Units = t.Units ?? 0,
+                              Store_region = t.Store_region,
+                              Week_num = t.Week_num ?? 0,
+                              Year = t.Year ?? 0,
+                              Department = p.Department,
+                              Commodity = p.Commodity,
+                              Loyalty_flag = h.Loyalty_flag,
+                              Age_range = h.Age_range,
+                              Marital_status = h.Marital_status,
+                              Income_range = h.Income_range,
+                              Homeowner_desc = h.Homeowner_desc,
+                              Hshd_composition = h.Hshd_composition,
+                              Hshd_size = h.Hshd_size ?? 0,
+                              Children = h.Children
+                          }).ToListAsync();
+        }
+
         public async Task<List<JoinedTransaction>> GetDataPullAsync(int hshdNum)
         {
             using var context = _dbFactory.CreateDbContext();
--- KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
+++ KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5627a613045ea0f647c3aa020d80d4ace15e0877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449f68d824366d842ef6009b86ab8d9d8bf566da")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
