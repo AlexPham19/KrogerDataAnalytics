b48c154e40c2f015fb45a26a21b96241fe8e19fb
Quân Phạm (Alex Pham) <phamqh@mail.uc.edu>
2026-04-25 20:05:44 -0400

    fix display

--- KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
+++ KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120ad1bab9dfe68aac623945f250a82f730ba3e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a72e98403ed29e7a3dff576236b8cba726707f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
