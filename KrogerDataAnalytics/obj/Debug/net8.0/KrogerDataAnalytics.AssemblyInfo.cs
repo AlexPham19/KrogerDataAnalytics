d6f147e36a563ffe62f4c54fafe6da86567b83e9
Quân Phạm (Alex Pham) <phamqh@mail.uc.edu>
2026-04-25 20:17:05 -0400

    Revert "."
    This reverts commit 457c34ce491dd8d42b0dfa506f419ead3c2b7c39.

--- KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
+++ KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7e96b5cd637db05dd9a2756bc378d6fc56ba08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d843115f312ecd7d9f2dc80e266e4f935db96ea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
