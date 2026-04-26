0387f0606a892189ed81a61c455b1511ebac2fcb
Quân Phạm (Alex Pham) <phamqh@mail.uc.edu>
2026-04-25 20:44:44 -0400

    delete

--- KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
+++ KrogerDataAnalytics/obj/Debug/net8.0/KrogerDataAnalytics.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a72e98403ed29e7a3dff576236b8cba726707f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1350912c1e170ea46444b280c0825abca3a7113")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrogerDataAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
